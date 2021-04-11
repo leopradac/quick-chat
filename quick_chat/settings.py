a569720407f71199dc4b9ad16a864bc7f6beba1f
Leo Prada <leo.prada90@gmail.com>
2021-04-11 01:01:32 -0500

    Add DRF

--- quick_chat/settings.py
+++ quick_chat/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
